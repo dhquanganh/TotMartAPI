1af55e67095a83eea7c28c450dbb3b039ed74a02
Chjchj123 <quanganh2004dasuo@gmail.com>
2026-04-29 21:35:32 +0700

    subcribe cart fix

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -246,6 +246,46 @@ class CartController {
             next(error);
         }
     }
+
+    async addSubcribePlanToCart(req, res, next) {
+        try {
+            const { subcricePlanId, quantity } = req.body;
+            const subcribePlan = await SubcribePlan.findOne({ _id: subcricePlanId });
+            if (!subcribePlan) {
+                return res.status(404).json({
+                    success: false,
+                    message: 'Subcribe plan not found for this product'
+                });
+            }
+            const cart = await Cart.findOne({ userId: req.user._id, isSubcribeCart: true }).populate('items.productId', 'name price');
+            if (cart) {
+                const subcribeItem = cart.items.find(item => item.subcricePlanId.toString() === subcricePlanId);
+                cart.totalPrice += quantity * subcribePlan.price;
+                if (subcribeItem) {
+                    subcribeItem.quantity += quantity;
+                }
+                else {
+                    cart.items.push({ subcricePlanId, quantity });
+                }
+                await cart.save();
+                res.status(200).json({
+                    success: true,
+                    message: 'Item added to subscribe cart successfully',
+                    data: cart
+                });
+            } else {
+                const newCart = new Cart({ userId: req.user._id, items: [{ subcricePlanId, quantity }], totalPrice: quantity * subcribePlan.price, isSubcribeCart: true });
+                await newCart.save();
+                res.status(201).json({
+                    success: true,
+                    message: 'Subscribe cart created successfully',
+                    data: newCart
+                });
+            }
+        } catch (error) {
+            next(error);
+        }
+    }
 }
 
 module.exports = new CartController();
